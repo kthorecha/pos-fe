f1114684102550ad4e985a77883777d31e80e08a
karan thorecha <kthorecha@gmail.com>
2021-05-07 01:06:06 +0530

    minor changes

--- src/app/components/product-sale/product-sale.component.ts
+++ src/app/components/product-sale/product-sale.component.ts
@@ -8,13 +8,28 @@ import { Component, OnInit } from '@angular/core';
 export class ProductSaleComponent implements OnInit {
 
   productCart: any[] = [
-    { product: 'sweater', price: 200, quantity: 2, total: 2000},
-    { product: 'apple', price: 200, quantity: 2, total: 2000},
-    { product: 'computer', price: 200, quantity: 2, total: 2000},
+    { product: 'sweater', price: 200, quantity: 2, total: 400},
+    { product: 'apple', price: 150, quantity: 2, total: 300},
+    { product: 'computer', price: 100, quantity: 2, total: 200},
   ];
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  updateProductCart(type: string, index: number) {
+    console.log(this.productCart)
+    if (type == 'plus') {
+      this.productCart[index]['quantity'] += 1;
+      this.productCart[index]['total'] += this.productCart[index]['price'];
+    } else {
+      if (this.productCart[index]['quantity'] > 0) {
+        this.productCart[index]['quantity'] -= 1;
+        this.productCart[index]['total'] -= this.productCart[index]['price'];
+      } else {
+        delete this.productCart[index];
+      }
+    }
   }
 
 }
